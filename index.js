5a26664dea6cec33719ae04f43c264c66505cecc
vishaliims <vishalwaghmare673@gmail.com>
2024-07-03 10:49:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Team!\n'
+  const msg = 'Hi Team!\n'
   res.end(msg);
 });
 
